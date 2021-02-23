e2e1d0f2a556864ef5fca50cdbca9ecd006ea207
Aparna Rao <pr.aparna@gmail.com>
2021-02-23 11:37:26 -0500

    Checking for config at both top level and bidder level

--- test/spec/modules/33acrossBidAdapter_spec.js
+++ test/spec/modules/33acrossBidAdapter_spec.js
@@ -1427,7 +1427,7 @@ describe('33acrossBidAdapter:', function () {
       it('builds a single request with multiple imps corresponding to each group {siteId, productId}', function() {
         sandbox.stub(config, 'getConfig').callsFake(() => {
           return {
-            enableSRAMode: 'true'
+            enableSRAMode: true
           }
         });
 
