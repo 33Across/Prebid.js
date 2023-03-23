9df6e1691b679fec168d80b1428833d263c5041a
Carlos Felix <carlos.felix@33across.com>
2023-03-23 11:22:24 -0500

    Send GPP parameters to 33across ID system endpoint

--- modules/33acrossIdSystem.js
+++ modules/33acrossIdSystem.js
@@ -8,7 +8,7 @@
 import { logMessage, logError } from '../src/utils.js';
 import { ajaxBuilder } from '../src/ajax.js';
 import { submodule } from '../src/hook.js';
-import { uspDataHandler } from '../src/adapterManager.js';
+import { uspDataHandler, gppDataHandler } from '../src/adapterManager.js';
 import { getStorageManager } from '../src/storageManager.js';
 
 const MODULE_NAME = '33acrossId';
@@ -42,6 +42,8 @@ function calculateResponseObj(response) {
 function calculateQueryStringParams(pid, gdprConsentData) {
   const uspString = uspDataHandler.getConsentData();
   const gdprApplies = Boolean(gdprConsentData?.gdprApplies);
+  const gppConsent = gppDataHandler.getConsentData();
+
   const params = {
     pid,
     gdpr: Number(gdprApplies),
@@ -51,6 +53,13 @@ function calculateQueryStringParams(pid, gdprConsentData) {
 
   if (uspString) {
     params.us_privacy = uspString;
+  }
+
+  if (gppConsent) {
+    const { gppString = '', applicableSections = [] } = gppConsent;
+
+    params.gpp = gppString;
+    params.gpp_sid = encodeURIComponent(applicableSections.join(','))
   }
 
   if (gdprApplies) {
